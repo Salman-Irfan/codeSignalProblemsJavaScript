2f86bb8597c9cea3ac832f00ea085005ea071571
Salman Irfan <salmanirfan674@gmail.com>
2023-05-12 09:02:28 +0500

    question description added

--- intro/chapter7DivingDeeper/differentSymbolsNaive.js
+++ intro/chapter7DivingDeeper/differentSymbolsNaive.js
@@ -1,3 +1,14 @@
+// Given a string, find the number of different characters in it.
+
+// Example
+
+// For s = "cabca", the output should be
+// solution(s) = 3.
+
+// There are 3 different characters a, b and c.
+// Guaranteed constraints:
+// 3 ≤ s.length ≤ 1000.
+
 function solution(s) {
 	// constraints
 	if (3 <= s.length && s.length <= 1000) {
