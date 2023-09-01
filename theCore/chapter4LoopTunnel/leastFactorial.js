13ed3e0906058e620781e795d9698b6d23024237
Salman-Irfan <salmanirfan674@gmail.com>
2023-09-01 09:33:40 -0700

    least factorial, n=17, return = 24, 1*2*3*4 = 24

---
+++ theCore/chapter4LoopTunnel/leastFactorial.js
@@ -0,0 +1,22 @@
+// Given an integer n, find the minimal k such that
+
+// k = m! (where m! = 1 * 2 * ... * m) for some integer m;
+// k >= n.
+// In other words, find the smallest factorial which is not less than n.
+
+// Example
+
+// For n = 17, the output should be
+// solution(n) = 24.
+
+// 17 < 24 = 4! = 1 * 2 * 3 * 4, while 3! = 1 * 2 * 3 = 6 < 17).
+
+function solution(n) {
+    let k = 1;
+    let m = 1;
+    while (k < n) {
+        m++;
+        k *= m;
+    }
+    return k;
+}
